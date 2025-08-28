6f38c8c2d98d952fae377239bd00ae9fa8af8cde
AndrejZaf <andrej.zafirovski@students.finki.ukim.mk>
2025-08-29 00:20:38 +0200

    Fix lint errors

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,72 +2,104 @@
 
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { MenuIcon } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const navigation = [
-    {
-        href: "/",
-        label: "home",
-    },
-    {
-        href: "/pokedex",
-        label: "pokedex",
-    },
-    {
-        href: "/about",
-        label: "about",
-    },
+  {
+    href: "/",
+    label: "home",
+  },
+  {
+    href: "/pokedex",
+    label: "pokedex",
+  },
+  {
+    href: "/about",
+    label: "about",
+  },
 ];
 
 const Navbar = () => {
-    const currentPath = usePathname();
-    return (
-        <div className="container mt-4 mb-4 mx-auto flex items-center justify-between px-4 py-2 md:py-0">
-            <Sheet>
-                <SheetTrigger asChild>
-                    <Button variant="outline" size="icon" className="md:hidden">
-                        <MenuIcon className="h-6 w-6" />
-                        <span className="sr-only">Toggle navigation menu</span>
-                    </Button>
-                </SheetTrigger>
-                <SheetContent side="left">
-                    <VisuallyHidden>
-                        <SheetHeader>
-                            <SheetTitle>Edit profile</SheetTitle>
-                            <SheetDescription>
-                                Make changes to your profile here. Click save when you're done.
-                            </SheetDescription>
-                        </SheetHeader>
-                    </VisuallyHidden>
-                    <div className="grid p-4">
-                        {navigation.map((item) =>
-                            <Link key={item.label} href={item.href}>
-                                <div style={{borderBottom: currentPath === item.href ? "4px solid #f87171" : ""}} className={`transition-all delay-75 text-lg py-2 border-b-4 border-transparent hover:border-red-300 capitalize ${currentPath === item.href && "border-red-400 hover:border-red-400"}`}>
-                                    {item.label}
-                                </div>
-                            </Link>)}
-                    </div>
-                </SheetContent>
-            </Sheet>
-            <Link href="/" className="flex items-center gap-2">
-                <img className="h-12 w-12" alt="Pokedex Logo" src="/pokeball-logo.svg" />
-            </Link>
-            <div className="hidden md:flex gap-4">
-                {navigation.map((item) =>
-                    <Link key={item.label} href={item.href}>
-                        <div style={{borderBottom: currentPath === item.href ? "4px solid #f87171" : ""}} className={`transition-all delay-75 text-lg py-2 border-b-4 border-transparent hover:border-red-300 capitalize ${currentPath === item.href && "border-red-400 hover:border-red-400"}`}>
-                            {item.label}
-                        </div>
-                    </Link>)}
+  const currentPath = usePathname();
+  return (
+    <div className="container mt-4 mb-4 mx-auto flex items-center justify-between px-4 py-2 md:py-0">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" size="icon" className="md:hidden">
+            <MenuIcon className="h-6 w-6" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <VisuallyHidden>
+            <SheetHeader>
+              <SheetTitle>Edit profile</SheetTitle>
+              <SheetDescription>
+                Make changes to your profile here. Click save when you&apos;re
+                done.
+              </SheetDescription>
+            </SheetHeader>
+          </VisuallyHidden>
+          <div className="grid p-4">
+            {navigation.map((item) => (
+              <Link key={item.label} href={item.href}>
+                <div
+                  style={{
+                    borderBottom:
+                      currentPath === item.href ? "4px solid #f87171" : "",
+                  }}
+                  className={`transition-all delay-75 text-lg py-2 border-b-4 border-transparent hover:border-red-300 capitalize ${
+                    currentPath === item.href &&
+                    "border-red-400 hover:border-red-400"
+                  }`}
+                >
+                  {item.label}
+                </div>
+              </Link>
+            ))}
+          </div>
+        </SheetContent>
+      </Sheet>
+      <Link href="/" className="flex items-center gap-2">
+        <img
+          className="h-12 w-12"
+          alt="Pokedex Logo"
+          src="/pokeball-logo.svg"
+        />
+      </Link>
+      <div className="hidden md:flex gap-4">
+        {navigation.map((item) => (
+          <Link key={item.label} href={item.href}>
+            <div
+              style={{
+                borderBottom:
+                  currentPath === item.href ? "4px solid #f87171" : "",
+              }}
+              className={`transition-all delay-75 text-lg py-2 border-b-4 border-transparent hover:border-red-300 capitalize ${
+                currentPath === item.href &&
+                "border-red-400 hover:border-red-400"
+              }`}
+            >
+              {item.label}
             </div>
-            <div className="flex">
-                <ModeToggle />
-            </div>
-        </div>
-    );
+          </Link>
+        ))}
+      </div>
+      <div className="flex">
+        <ModeToggle />
+      </div>
+    </div>
+  );
 };
 export default Navbar;
--- components/pokedex/pokedex-grid.tsx
+++ components/pokedex/pokedex-grid.tsx
@@ -9,46 +9,52 @@ import { useInView } from "react-intersection-observer";
 import useSWRInfinite from "swr/infinite";
 
 const fetcher = async (url: string) => {
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error("Network error");
-    }
-    return response.json();
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Network error");
+  }
+  return response.json();
 };
 
-const getKey = (pageIndex: number, previousPageData: any) => {
-    if (previousPageData && !previousPageData.next) {
-        return null;
-    }
+const getKey = (
+  pageIndex: number,
+  previousPageData: { next?: string } | null
+) => {
+  if (previousPageData && !previousPageData.next) {
+    return null;
+  }
 
-    const offset = pageIndex * 16;
-    return `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=16`;
+  const offset = pageIndex * 16;
+  return `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=16`;
 };
 
 const PokedexGrid = () => {
-    const { ref, inView } = useInView();
-    const { data, setSize, isValidating, isLoading } = useSWRInfinite(getKey, fetcher);
-    useEffect(() => {
-        if (inView && !isValidating && !isLoading) {
-            setSize((prevSize) => prevSize + 1);
-        }
-    }, [inView, isValidating, setSize, isLoading]);
-    return (
-        <>
-            <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 mt-4 px-8">
-                {data?.map((page) =>
-                    page.results.map((pokemon: PokemonMinified) => {
-                        const index = getIndex(pokemon.url);
-                        return (
-                            <Link key={pokemon.name} href={`/pokemon/${index}`}>
-                                <PokemonCard index={index} pokemon={pokemon} />
-                            </Link>
-                        );
-                    }),
-                )}
-            </div>
-            <div ref={ref}>Load more</div>
-        </>
-    );
+  const { ref, inView } = useInView();
+  const { data, setSize, isValidating, isLoading } = useSWRInfinite(
+    getKey,
+    fetcher
+  );
+  useEffect(() => {
+    if (inView && !isValidating && !isLoading) {
+      setSize((prevSize) => prevSize + 1);
+    }
+  }, [inView, isValidating, setSize, isLoading]);
+  return (
+    <>
+      <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 mt-4 px-8">
+        {data?.map((page) =>
+          page.results.map((pokemon: PokemonMinified) => {
+            const index = getIndex(pokemon.url);
+            return (
+              <Link key={pokemon.name} href={`/pokemon/${index}`}>
+                <PokemonCard index={index} pokemon={pokemon} />
+              </Link>
+            );
+          })
+        )}
+      </div>
+      <div ref={ref}>Load more</div>
+    </>
+  );
 };
 export default PokedexGrid;
